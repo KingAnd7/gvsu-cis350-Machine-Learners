8c789c9c4ea7ec393068d9a340e5fa46c6b90876
KingAnd7 <aahills04@gmail.com>
2024-09-23 11:44:15 -0400

    Class and Exercise added
    Added a Exercise class that has a name, sets, reps, muscle groups, and a summary. also added a example exercise

--- src/src.py
+++ src/src.py
@@ -5,3 +5,14 @@ root.title("Workout Program")
 label = tk.Label(root, text="This is a workout program")
 label.pack()
 root.mainloop()
+
+class Exercise:
+    def __init__(self, name, sets, reps, muscle_group1, muscle_group2, summary) -> None:
+        self.name
+        self.sets
+        self.reps
+        self.muscle_group1
+        self.muscle_group2
+        self.summary
+
+bench_press = Exercise("Bench Press", 0, 0, "Chest", "N/A", "Bench Press a bar")
