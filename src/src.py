152f9da1d979e7106dbbae8cffa2c44bce4d4c68
preston-thomas <140858770+preston-thomas@users.noreply.github.com>
2024-09-25 11:41:06 -0400

    Modifying Exercise constructor, instance vars
    Just a few modifications for the exercise class. Will try to wrap this up by next meeting.

--- src/src.py
+++ src/src.py
@@ -7,12 +7,20 @@ label.pack()
 root.mainloop()
 
 class Exercise:
-    def __init__(self, name, sets, reps, muscle_group1, muscle_group2, summary) -> None:
-        self.name
-        self.sets
-        self.reps
-        self.muscle_group1
-        self.muscle_group2
-        self.summary
+    def __init__(self, name="None", sets=0, reps=0, muscle_group1="None", muscle_group2="None", summary='') -> None:
+        self.name = name
+        self.sets = sets
+        self.reps = reps
+        self.muscle_group1 = muscle_group1
+        self.muscle_group2 = muscle_group2
+        self.summary = summary
 
-bench_press = Exercise("Bench Press", 0, 0, "Chest", "N/A", "Bench Press a bar")
+# TO DO: Need to implement getters, setters for each instance variable
+
+bench_press = Exercise("Barbell Bench Press", 0, 0, "Chest", "Triceps, Front Deltoids", "")
+inc_bench_press = Exercise("Incline Barbell Bench Press", 0, 0, "Chest", "Triceps, Front Deltoids", "")
+chest_press = Exercise("Chest Press", 0, 0, "Chest", "Triceps, Front Deltoids", "")
+dumbbell_fly = Exercise("Dumbell Fly", 0, 0, "Chest", "Front Deltoids", "")
+cable_fly = Exercise("Cable Fly", 0, 0, "Chest", "Front Deltoids", "")
+dips = Exercise("Dips", 0, 0, "Chest", "Triceps, Deltoids", "")
+tricep_pushdown = Exercise("Tricep Pushdown", muscle_group1="Triceps", summary='')
